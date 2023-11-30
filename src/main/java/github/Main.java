3d59ed3646da881d51fbc89ed4bc939b6b7457d1
JoseLuisCandela <152269935+JoseLuisCandela@users.noreply.github.com>
2023-11-30 09:29:08 +0100

    Update Main.java

--- src/main/java/github/Main.java
+++ src/main/java/github/Main.java
@@ -2,6 +2,6 @@ package github;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("esto es una prueba para github");
+        System.out.println("hola mundo");
     }
 }
